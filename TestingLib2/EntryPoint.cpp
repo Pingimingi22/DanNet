edc193c7e1a60bf0be287e33dfdeca39c08ea284
Dan <daniel@ac.com.au>
2021-05-02 22:56:04 +1000

    * Tested if packet's arrived complete.

--- TestingLib2/EntryPoint.cpp
+++ TestingLib2/EntryPoint.cpp
@@ -1,6 +1,13 @@
 #include "Peer.h"
 #include "Packet.h"
 
+
+struct TestStruct
+{
+	int foo = 0;
+	int bar = 0;
+
+};
 int main()
 {
 	Peer testPeer = Peer(true, 25565);
@@ -13,6 +20,11 @@ int main()
 		Packet* testPacket = testPeer.UDPReceivePacket();
 		if (testPacket != nullptr)
 		{
+			
+			TestStruct testStruct;
+			testPacket->Deserialize(testStruct.foo, testStruct.bar);
+			std::cout << testStruct.foo << std::endl;
+
 			std::cout << "received message" << std::endl;
 			isRunning = false;
 		}
